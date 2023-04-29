e4b7dc8540f8fd7b0702d730abcbc344375fb3ab
HTanoda <131184857+HTanoda@users.noreply.github.com>
2023-04-29 22:17:00 +0900

    Add files via upload

--- FukHpapp.py
+++ FukHpapp.py
@@ -22,7 +22,7 @@ def generate_search_query(input_keyword):
 @st.cache
 def get_search_results(search_query):
     # サイト内検索のURL
-    search_url = f"https://www.city.fukuoka.lg.jp/search/search.html?q={search_query}"
+    search_url = f"https://www.city.fukuoka.lg.jp/search/search.html?q={search_query}#gsc.tab=0&gsc.q={search_query}&gsc.page=1"
     
     # ページの内容を取得
     page = requests.get(search_url)
@@ -50,10 +50,10 @@ def summarize_content(content):
     return completion['choices'][0]['message']['content']
 
 # Streamlitアプリの設定
-st.title('Search Query Generator and Summarizer')
+st.title('福岡市ホームページ検索')
 input_keyword = st.text_input('Enter a keyword')
 
-if st.button('Generate and Summarize'):
+if st.button('検索ぅ！'):
     search_query = generate_search_query(input_keyword)
     search_results = get_search_results(search_query)
     summary = summarize_content(search_results)
